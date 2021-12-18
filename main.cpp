27ca07c923f6200598b39f3a6f373d4e033b6e96
Boinga15 <38063428+Boinga15@users.noreply.github.com>
2021-12-18 16:54:56 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-/*
-Implement:
--CPU to handle instruction processing.
--GPU to handle outputting things to the screen.
--RAM to hold instruction list (+instruction set).
--ROM to hold boot-up instructions.
-*/
-
 #include<iostream>
 #include "cpu.h"
 #include "ram.h"
